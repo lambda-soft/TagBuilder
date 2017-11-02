bb2cc0af4017bc7700cdfce2a4610c01b4c6ecd6
Spencer@Lambdasoft <lambdasoftware@outlook.com>
2017-11-02 11:42:10 -0400

    Created collapsed navigation

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,6 +10,14 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('init');
+    $(document).ready(d =>{
+      $(document).click(event =>{
+        let isOpened = $('.navbar-collapse').hasClass('collapse in');
+        if(isOpened)
+          $('button.navbar-toggle').click();
+      })
+    })
   }
 
 }
